83284e84698bf23fe2fc0008c73abe4fe9f8b27a
woleigh <woleigh2000@gmail.com>
2022-01-09 02:38:01 -0500

    change

--- src/python/helloworld/helloworld.py
+++ src/python/helloworld/helloworld.py
@@ -1,5 +1,5 @@
-from mask import Flask, request
-from mask_restful import Resource, Api
+#from flask import Flask, request
+#from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
